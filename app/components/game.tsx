570b179036309331f62d1f885efdae287592a4e3
Ricky Marasigan <rickysoliman@gmail.com>
2026-05-13 12:35:31 -0400

    change space background & style & make letters visible

--- app/components/game.tsx
+++ app/components/game.tsx
@@ -1,11 +1,17 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 
 export default function Game() {
+  const [mostRecentKey, setMostRecentKey] = useState("");
+
   useEffect(() => {
     window.addEventListener("keydown", (e) => {
-      console.log(e);
+      setMostRecentKey(e.key);
     });
   }, []);
 
-  return <div>Game Component</div>;
+  return (
+    <>
+      {mostRecentKey && <div className="most-recent-key">{mostRecentKey}</div>}
+    </>
+  );
 }
